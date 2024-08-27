e97f665a2a9d4db3e55a81dff5129e738e770240
Shoriful-Shuvo <shuvo696727xyz@gmail.com>
2024-08-27 20:04:45 +0600

    object method,arrow function

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let info =  {
+    name : "shoriful",
+    age : 21,
+    district : "barisal",
+    methd :function salary() {
+       let  month = 100000;
+       let year = 12;
+       let salary =month*year ;
+       console.log(salary);
+       
+        
+    },
+
+};
+info.methd()
+
+
+
+// object r moddhe method holo just akta variable r moddhe function ke call kora 
+// mone rakhba je object r modhhe property gulo : , agula die likhba kron agula property ar sudu matro variable r jonno =; agula likhba
+
+
+
+// arrow function
+
+// let shuvo =()=>{
+//     console.log("amr nam shuvo");
+    
+// }
+// shuvo()
